2d3ebaeaa971035e52111d8dfe2d867d37f7044d
Marcelo Oliveira <marcelotk_15@hotmail.com>
2021-01-11 14:27:31 -0300

    📦 NEW: home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <div className="container">
+        <div className="card">
+          <div className="card-header">
+            <p>Codeby</p>
+          </div>
+
+          <div className="card-body">
+            <div className="home-links">
+                <button><Link to="/pagar/acima" >Carrinho acima de $10,00</Link></button>
+                <button><Link to="/pagar/abaixo" >Carrinho abaixo de $10,00</Link></button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
