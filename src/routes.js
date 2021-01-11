1fb13631ecf78fad5be34d75c51ae808e56101ca
Marcelo Oliveira <marcelotk_15@hotmail.com>
2021-01-11 14:27:10 -0300

    📦 NEW: cart route

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Home from './pages/home'
+import Cart from './pages/Cart'
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path='/pagar/:cart' component={Cart} />
       </Switch>
     </BrowserRouter>
   )
